0c2f597ecc5764ee0dda0c8848792aa82d2346e9
Pavlos Melissinos <pmelissi@gmail.com>
2020-10-06 10:16:31 +0300

    Add lockkeys indicator

---
+++ .config/waybar/lockkeys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+capslock_active=`cat /sys/class/leds/input24::capslock/brightness`
+numlock_active=`cat /sys/class/leds/input24::numlock/brightness`
+
+capslock_text=""
+if [[ "$capslock_active" != "0" ]]; then
+    capslock_text="⬆"
+fi
+
+numlock_text=""
+if [[ "$numlock_active" != "0" ]]; then
+    numlock_text="1"
+fi
+
+if [[ "$capslock_text" != "" || "$numlock_text" != "" ]]; then
+    echo '{"text": "'$capslock_text$numlock_text'","tooltip":"$tooltip","class":"$class"}'
+fi
