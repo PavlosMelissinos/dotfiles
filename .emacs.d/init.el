3e1e8b4595547e0a7d5f6a0b4b53e59747623041
Pavlos Melissinos <pmelissi@gmail.com>
2021-03-11 00:47:36 +0200

    Add org-roam to git

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -602,6 +602,22 @@
   :init
   (org-tree-slide-simple-profile))
 
+(use-package org-roam
+  :ensure t
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/notes/roam")
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate)))
+  ;;(add-hook 'after-init-hook 'org-roam-mode)
+  )
+
 (use-package org-bullets
   :ensure t
   :init
