d2b673b0100cd683e564c70964e76f8d58950a52
Pavlos Melissinos <pavlos@baresquare.com>
2022-02-26 15:03:53 +0200

    [guix] Update channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,35 +3,34 @@
 ;; used to populate this profile.
 
 (list
-     (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit
-        "52269e27c628d34af896e95347870b1755cb3e5f")
-       (introduction
-         (make-channel-introduction
-           "9edb3f66fd807b096b48283debdcddccfea34bad"
-           (openpgp-fingerprint
-             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-     (channel
-      (name 'flat)
-      (url "https://github.com/flatwhatson/guix-channel.git")
-      (commit
-       "ece373bcd1e00ef746fb7b104e56ab610cc4af2a")
-      (introduction
-       (make-channel-introduction
-        "33f86a4b48205c0dc19d7c036c85393f0766f806"
-        (openpgp-fingerprint
-         "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
-     (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (commit
-         "35533c763a81365e14d0b58ad262e8f88c82872c")
-        ;; Enable signature verification:
-        (introduction
-         (make-channel-introduction
-          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-          (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-)
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (commit
+   "07c3a439555113bdd0bd416265221063d263f44c")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (branch "master")
+  (commit
+   "3ae6f63be19782c835f52f29eb44731951a72069")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+ (channel
+  (name 'guix-gaming-games)
+  (url "https://gitlab.com/guix-gaming-channels/games.git")
+  ;; Enable signature verification:
+  (introduction
+   (make-channel-introduction
+    "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+    (openpgp-fingerprint
+     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+ )
