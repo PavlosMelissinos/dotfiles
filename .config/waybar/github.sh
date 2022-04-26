d3deea217de0b4bc9adfe6696680512c29d0a58b
Pavlos Melissinos <pavlos@baresquare.com>
2022-04-26 22:41:52 +0300

    [waybar] Add repo name and PR title to github module tooltip

--- .config/waybar/github.sh
+++ .config/waybar/github.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 token=`cat ${HOME}/.config/github/notifications.token`
-count=`curl -u PavlosMelissinos:${token} https://api.github.com/notifications | jq '. | length'`
+notifications=`curl -u PavlosMelissinos:${token} https://api.github.com/notifications`
+count=`echo $notifications | jq '. | length'`
+tooltip=`echo $notifications | jq '. | map({"title": .subject | .title, "repo": .repository | .full_name} | "(" + .repo + ") " + .title) | join("\n\n")'`
+class=0
 
 if [[ "$count" != "0" ]]; then
-    echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
+    echo '{"text":'$count',"tooltip":'$tooltip',"class":'$class'}' | jq --unbuffered --compact-output
 fi
