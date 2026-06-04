0d89539fc6469c098adfd720feb3f6cc98b252bb
Pavlos <pmelissi@gmail.com>
2026-06-04 23:55:56 +0300

    nixos: Bump to 25.11, add dotfiles input for read-only home directory
    - Bump nixpkgs and home-manager from 25.05 to 25.11
    - Add dotfiles input pointing to PavlosMelissinos/dotfiles
    - Pass dotfiles through extraSpecialArgs for home.nix to use as
      managed file source instead of live filesystem

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -2,18 +2,22 @@
   description = "NixOS system configuration for pavlos@localhost";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixgl = {
       url = "github:nix-community/nixGL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dotfiles = {
+      url = "github:PavlosMelissinos/dotfiles";
+      flake = false;  # Treat as source, not flake
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixgl }: {
+  outputs = { self, nixpkgs, home-manager, nixgl, dotfiles }: {
     nixosConfigurations.localhost-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -23,7 +27,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.pavlos = import ../home-manager/home.nix;
-          home-manager.extraSpecialArgs = { inherit nixgl; };
+          home-manager.extraSpecialArgs = { inherit nixgl dotfiles; };
         }
       ];
     };
